0ba8bd781ebb55f3c1f0e62e7d1dfceab51070b6
frique92 <29735207+frique92@users.noreply.github.com>
2020-04-14 09:50:33 +0300

    complete "Bubble sort" in the 5 stage

---
+++ Problems/Swaps/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] tempArr = scanner.nextLine().split(" ");
+
+        int[] array = new int[tempArr.length];
+
+        for (int i = 0; i < tempArr.length; i++) {
+            array[i] = Integer.parseInt(tempArr[i]);
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+
+    }
+}
